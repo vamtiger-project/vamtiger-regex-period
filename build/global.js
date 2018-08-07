ab58d11b4416d04b8e5589a095e56c04b2e31928
James-Alex Matthews <vamtiger@vamtiger.com>
2018-08-08 01:28:57 +0200

    Clean

--- build/global.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const xregexp = require("xregexp");
-const pattern_1 = require("./pattern");
-const XRegExp = xregexp;
-const flags = 'g';
-exports.default = XRegExp(pattern_1.default, flags);
-//# sourceMappingURL=global.js.map
--- build/regex.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const xregexp = require("xregexp");
-const pattern_1 = require("./pattern");
-exports.XRegExp = xregexp;
-exports.default = exports.XRegExp(pattern_1.default);
-//# sourceMappingURL=regex.js.map
