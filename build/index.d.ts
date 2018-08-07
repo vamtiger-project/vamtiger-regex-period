c6d986cc1b0a98ae0a44ef6767570757faf5fd89
James-Alex Matthews <vamtiger@vamtiger.com>
2018-08-08 01:25:36 +0200

    clean

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as regex } from './regex';
-export { default as globalRegex } from './global';
-export { default as multilineRegex } from './multiline';
-export { default as globalMultilineRegex } from './global-multiline';
--- build/multiline.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const xregexp = require("xregexp");
-const pattern_1 = require("./pattern");
-const XRegExp = xregexp;
-const flags = 'm';
-exports.default = XRegExp(pattern_1.default, flags);
-//# sourceMappingURL=multiline.js.map
